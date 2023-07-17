97e8229a4c2166e3f3d05c32533e42218534d723
muasya <muasyamumina@gmail.com>
2023-07-17 21:06:04 +0300

    Dom content loading

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Your code goes here
+// Adding event listener 
+document.addEventListener("DOMContentLoaded", function() {
+  console.log("The DOM has loaded");
+});
+
+console.log("The DOM content loads");
+
+window.addEventListener("DOMContentLoaded", function() {
+    const paragraph = document.getElementById("text");
+    paragraph.textContent = "This is really cool!";
+  });
